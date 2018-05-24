e836cca03feae2d42fd1b950fd5ebe374430a8f6
Artem Anashchenkov <anashchenkov@g5software.com>
2018-05-24 18:50:47 +0300

    Add slider and description to the room page

--- public/js/app.js
+++ public/js/app.js
@@ -1,16 +1,18 @@
-var $subscribeForm = $('.subscribe-form')
-$subscribeForm.on('submit', function (event) {
-    event.preventDefault()
-    var $emailField = $subscribeForm.find('.input-email')
-    var data = {
-        email: $emailField.val()
-    }
-    $.ajax('/api/subscriber', {
-        method: 'post',
-        data: data
-    }).then(function() {
-        $('.modal').modal('show')
-    },
-    function() {
+(function () {
+    var $subscribeForm = $('.subscribe-form')
+    $subscribeForm.on('submit', function (event) {
+        event.preventDefault()
+        var $emailField = $subscribeForm.find('.input-email')
+        var data = {
+            email: $emailField.val()
+        }
+        $.ajax('/api/subscriber', {
+            method: 'post',
+            data: data
+        }).then(function() {
+            $('.modal').modal('show')
+        },
+        function() {
+        })
     })
-})
+})()
